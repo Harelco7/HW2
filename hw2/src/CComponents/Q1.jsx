dc5c3fa816e6fa7016e5f43529ddb2ac5d007f83
Harelco7 <harelcohen7@gmail.com>
2024-01-26 15:57:52 +0200

    clean

--- hw2/src/CComponents/Q1.jsx
+++ hw2/src/CComponents/Q1.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const colors =['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Brown']
+const colors = [
+  "Red",
+  "Blue",
+  "Green",
+  "Yellow",
+  "Purple",
+  "Orange",
+  "Pink",
+  "Brown",
+];
 export default class CCButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      backgroundColor: '',
+      backgroundColor: "",
     };
   }
 
@@ -13,15 +22,17 @@ export default class CCButton extends Component {
     this.setState({ backgroundColor: color });
   };
 
-  
   render() {
-   
-
     return (
-      <div style={{ backgroundColor: this.state.backgroundColor, padding: '20px' }}>
-        
+      <div
+        style={{ backgroundColor: this.state.backgroundColor, padding: "20px" }}
+      >
         <div>
-          {colors.map( (color) => (<button style={{margin:10, background:color}} onClick={() => this.Click(color)}>
+          {colors.map((color) => (
+            <button
+              style={{ margin: 10, background: color }}
+              onClick={() => this.Click(color)}
+            >
               {color}
             </button>
           ))}
@@ -30,5 +41,3 @@ export default class CCButton extends Component {
     );
   }
 }
-
-
--- hw2/src/CComponents/Q3.jsx
+++ hw2/src/CComponents/Q3.jsx
@@ -1,54 +1,51 @@
-
 import { Component } from "react";
 
-export default class CCTable extends Component{
-    constructor(props){
-        super(props)
+export default class CCTable extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state={
-            width:0,
-        }
-        
-    }
+    this.state = {
+      width: 0,
+    };
+  }
 
+  Change50 = () => {
+    this.setState({
+      width: "50%",
+    });
+  };
+  Change100 = () => {
+    this.setState({
+      width: "100%",
+    });
+  };
 
-    Change50 = () => {
-        this.setState({
-            width : '50%'
-        })
-    }
-    Change100 = () => {
-        this.setState({
-            width : '100%'
-        }) 
-    }
-
-    render(){
-        return(
-            <div>
-                    <table border={1}
-                    onClick={this.Change50}
-                    onDoubleClick={this.Change100}
-                    style={{
-                        borderStyle: 'solid',
-                        borderColor: 'white',
-                        borderWidth: '3px',
-                        width:this.state.width
-
-                    }}>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                        </tr>
-                        
-                    </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <table
+          border={1}
+          onClick={this.Change50}
+          onDoubleClick={this.Change100}
+          style={{
+            borderStyle: "solid",
+            borderColor: "white",
+            borderWidth: "3px",
+            width: this.state.width,
+          }}
+        >
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
